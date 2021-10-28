43f47f39dbb6f6acd9b92b01269fe2b3ac7f5500
Hannah Gardner <82235793+hannahgardner96@users.noreply.github.com>
2021-10-28 11:04:07 -0500

    tests working for topping option

--- sundaes-on-demand/src/mocks/handlers.js
+++ sundaes-on-demand/src/mocks/handlers.js
@@ -9,4 +9,13 @@ export const handlers = [
       ])
     );
   }),
+  rest.get("https://localhost:3030/toppings", (req, res, ctx) => {
+    return res(
+      ctx.json([
+        { name: "Cherries", imagePath: "./images/cherries.png" },
+        { name: "Hot fudge", imagePath: "/images/hot-fudge.png" },
+        { name: "M&Ms", imagePath: "/images/m-and-ms.png" },
+      ])
+    );
+  }),
 ];
