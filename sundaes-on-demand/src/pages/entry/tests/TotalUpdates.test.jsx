225f43a642f1dce0041db81450bb43c4bbac9e1d
Hannah Gardner <82235793+hannahgardner96@users.noreply.github.com>
2021-10-28 16:33:13 -0500

    tests for toppings checkboxes and subtotal complete

--- sundaes-on-demand/src/pages/entry/tests/TotalUpdates.test.jsx
+++ sundaes-on-demand/src/pages/entry/tests/TotalUpdates.test.jsx
@@ -1,10 +1,10 @@
-import { render, screen } from "@testing-library/react";
+import { render, screen } from "../../../test-utils/testing-library-utils";
 import userEvent from "@testing-library/user-event";
 import { OrderDetailsProvider } from "../../../contexts/OrderDetails";
 import Options from "../Options";
 
 test("update scoop subtotal when scoops change", async () => {
-  render(<Options optionType="scoops" />, { wrapper: OrderDetailsProvider });
+  render(<Options optionType="scoops" />);
 
   const scoopsSubtotal = screen.getByText("Scoops total: $", { exact: false });
   expect(scoopsSubtotal).toHaveTextContent("0.00");
@@ -22,4 +22,27 @@ test("update scoop subtotal when scoops change", async () => {
   userEvent.clear(chocolateInput);
   userEvent.type(chocolateInput, "2");
   expect(scoopsSubtotal).toHaveTextContent("6.00");
+});
+
+test("update toppings subtotal when toppings change", (async) => {
+  render(<Options optionType="toppings" />);
+
+  const toppingsSubtotal = screen.getByText("Toppings total: $", {
+    exact: false,
+  });
+  expect(toppingsSubtotal).toHaveTextContent("0.00");
+
+  const cherriesInput = await screen.findByRole("checkbox", {
+    name: "Cherries",
+  });
+  const mAndMsInput = await screen.findByRole("checkbox", { name: "M&Ms" });
+
+  userEvent.click(cherriesInput);
+  expect(toppingsSubtotal).toHaveTextContent("1.50");
+
+  userEvent.click(mAndMsInput);
+  expect(toppingsSubtotal).toHaveTextContent("3.00");
+
+  userEvent.click(mAndMsInput);
+  expect(toppingsSubtotal).toHaveTextContent("1.50");
 });
