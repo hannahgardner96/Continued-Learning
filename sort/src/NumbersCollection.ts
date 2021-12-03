ea5026eb6e4d1bd83a2c31a4c899c496fa346b7f
Hannah Gardner <82235793+hannahgardner96@users.noreply.github.com>
2021-12-03 11:56:53 -0600

    section 10 complete

--- sort/src/NumbersCollection.ts
+++ sort/src/NumbersCollection.ts
@@ -1,5 +1,9 @@
-export class NumbersCollection {
-  constructor(public data: number[]) {}
+import { Sorter } from "./Sorter";
+
+export class NumbersCollection extends Sorter {
+  constructor(public data: number[]) {
+    super(); // calling super runs constructor on parent class, must pass necessary arguments
+  }
 
   get length(): number {
     return this.data.length;
